869bf629d3094f14b901fe2b72ab67a52d49de6f
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-08 21:14:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
 
+// Serve static files from 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default route: serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
